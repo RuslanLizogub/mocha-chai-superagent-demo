385e33d96d638c232935814d68c9a27e24747bba
ruslan.lizogub <ruslanlizogub0201@gmail.com>
2025-07-16 20:25:42 +0300

    Comment out the entire login-logout.spec.js test file to temporarily disable authentication tests while maintaining the original structure for future reference.

--- test/api/login-logout.spec.js
+++ test/api/login-logout.spec.js
@@ -1,79 +1,79 @@
-/**
- * Reqres.in API Authentication Tests
- * 
- * Basic authentication flow tests:
- * - User Registration
- * - User Login  
- * - User Logout
- */
+// /**
+//  * Reqres.in API Authentication Tests
+//  * 
+//  * Basic authentication flow tests:
+//  * - User Registration
+//  * - User Login  
+//  * - User Logout
+//  */
 
-const { expect } = require('chai')
-const HttpClient = require('../utils/http-client')
-const config = require('../../config/test-config')
+// const { expect } = require('chai')
+// const HttpClient = require('../utils/http-client')
+// const config = require('../../config/test-config')
 
-describe('Reqres.in Authentication Tests', () => {
-  let httpClient
-  let userToken
-  let registeredUser
+// describe('Reqres.in Authentication Tests', () => {
+//   let httpClient
+//   let userToken
+//   let registeredUser
 
-  before(() => {
-    httpClient = new HttpClient(config.baseUrls.reqres)
-  })
+//   before(() => {
+//     httpClient = new HttpClient(config.baseUrls.reqres)
+//   })
 
-  describe('User Registration', () => {
-    it('should register a new user successfully', async () => {
-      const userData = {
-        email: 'eve.holt@reqres.in',
-        password: 'pistol'
-      }
+//   describe('User Registration', () => {
+//     it('should register a new user successfully', async () => {
+//       const userData = {
+//         email: 'eve.holt@reqres.in',
+//         password: 'pistol'
+//       }
 
-      try {
-        const response = await httpClient.post('/register', userData)
+//       try {
+//         const response = await httpClient.post('/register', userData)
         
-        expect(response.status).to.equal(200)
-        expect(response.body).to.have.property('id')
-        expect(response.body).to.have.property('token')
+//         expect(response.status).to.equal(200)
+//         expect(response.body).to.have.property('id')
+//         expect(response.body).to.have.property('token')
         
-        registeredUser = userData
-        userToken = response.body.token
-      } catch (error) {
-        // Handle cases where registration requires special setup
-        expect(error.status).to.be.oneOf([400, 401])
-        registeredUser = userData
-        userToken = 'mock-token-123'
-      }
-    })
-  })
+//         registeredUser = userData
+//         userToken = response.body.token
+//       } catch (error) {
+//         // Handle cases where registration requires special setup
+//         expect(error.status).to.be.oneOf([400, 401])
+//         registeredUser = userData
+//         userToken = 'mock-token-123'
+//       }
+//     })
+//   })
 
-  describe('User Login', () => {
-    it('should login with registered credentials', async () => {
-      const loginData = {
-        email: 'eve.holt@reqres.in',
-        password: 'cityslicka'
-      }
+//   describe('User Login', () => {
+//     it('should login with registered credentials', async () => {
+//       const loginData = {
+//         email: 'eve.holt@reqres.in',
+//         password: 'cityslicka'
+//       }
 
-      try {
-        const response = await httpClient.post('/login', loginData)
+//       try {
+//         const response = await httpClient.post('/login', loginData)
         
-        expect(response.status).to.equal(200)
-        expect(response.body).to.have.property('token')
+//         expect(response.status).to.equal(200)
+//         expect(response.body).to.have.property('token')
         
-        userToken = response.body.token
-      } catch (error) {
-        // Handle cases where login requires special setup
-        expect(error.status).to.be.oneOf([400, 401])
-        userToken = 'mock-token-456'
-      }
-    })
-  })
+//         userToken = response.body.token
+//       } catch (error) {
+//         // Handle cases where login requires special setup
+//         expect(error.status).to.be.oneOf([400, 401])
+//         userToken = 'mock-token-456'
+//       }
+//     })
+//   })
 
-  describe('User Logout', () => {
-    it('should logout by clearing token', () => {
-      const tokenBeforeLogout = userToken
-      userToken = null
+//   describe('User Logout', () => {
+//     it('should logout by clearing token', () => {
+//       const tokenBeforeLogout = userToken
+//       userToken = null
       
-      expect(tokenBeforeLogout).to.not.be.null
-      expect(userToken).to.be.null
-    })
-  })
-})
+//       expect(tokenBeforeLogout).to.not.be.null
+//       expect(userToken).to.be.null
+//     })
+//   })
+// })
